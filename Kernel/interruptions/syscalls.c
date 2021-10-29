8f121e48792efca909d7f925ead79a7d2d2042d5
BraveJero <jbrave271@gmail.com>
2021-10-29 09:03:51 -0300

    clean up

--- Kernel/interruptions/syscalls.c
+++ Kernel/interruptions/syscalls.c
@@ -5,9 +5,10 @@
 #include <process.h>
 #include <scheduler.h>
 #include <stdint.h>
+#include <sync.h>
 #include <syscalls.h>
 #include <video.h>
-#include <sync.h>
+
 
 typedef uint64_t (*PSysCall)(uint64_t, uint64_t, uint64_t, uint64_t, uint64_t);
 
@@ -17,11 +18,12 @@ typedef struct dateType {
 } dateType;
 
 int64_t sys_read(unsigned int fd, char *buf, size_t count);
-uint64_t sys_write(uint8_t fd, char *buffer, uint64_t count);
+int64_t sys_write(uint8_t fd, char *buffer, uint64_t count);
 uint64_t sys_date(dateType *pDate);
 uint64_t sys_mem(uint64_t rdi, uint64_t rsi, uint8_t rdx);
 void sys_ps(void);
-pid_t sys_createPs(uint64_t rip, char *name, int argc, char *argv[], uint8_t mode);
+pid_t sys_createPs(uint64_t rip, char *name, int argc, char *argv[],
+                   uint8_t mode);
 int sys_block(pid_t pid);
 int sys_unblock(pid_t pid);
 int sys_kill(pid_t pid);
@@ -35,8 +37,8 @@ static PSysCall sysCalls[255] = {
     (PSysCall)&sys_mem,    (PSysCall)&sys_ps,      (PSysCall)&sys_createPs,
     (PSysCall)&sys_block,  (PSysCall)&sys_unblock, (PSysCall)&sys_kill,
     (PSysCall)&sys_getpid, (PSysCall)&sys_nice,    (PSysCall)&sys_exit,
-    (PSysCall)&sys_yield, (PSysCall)&sem_open, (PSysCall)&sem_post,    
-    (PSysCall)&sem_wait, (PSysCall)&sem_close};
+    (PSysCall)&sys_yield,  (PSysCall)&sem_open,    (PSysCall)&sem_post,
+    (PSysCall)&sem_wait,   (PSysCall)&sem_close};
 
 uint64_t sysCallDispatcher(uint64_t rdi, uint64_t rsi, uint64_t rdx,
                            uint64_t rcx, uint64_t r8, uint64_t rax) {
@@ -46,7 +48,7 @@ uint64_t sysCallDispatcher(uint64_t rdi, uint64_t rsi, uint64_t rdx,
   return 0;
 }
 
-uint64_t sys_write(uint8_t fd, char *buffer, uint64_t count) {
+int64_t sys_write(uint8_t fd, char *buffer, uint64_t count) {
   if (buffer == NULL || count == 0)
     return -1;
 
@@ -111,7 +113,8 @@ uint64_t sys_mem(uint64_t rdi, uint64_t rsi, uint8_t rdx) {
 
 void sys_ps(void) { showAllPs(); }
 
-pid_t sys_createPs(uint64_t rip, char *name, int argc, char *argv[], uint8_t mode) {
+pid_t sys_createPs(uint64_t rip, char *name, int argc, char *argv[],
+                   uint8_t mode) {
   return createProcess(rip, 0, name, argc, argv, mode);
 }
 
