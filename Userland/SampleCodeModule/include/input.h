66b076f9fcf477e686501381beb146609c04b0f0
juan <juanigarcia@itba.edu.ar>
2021-10-31 19:58:21 -0300

    Add new file.

---
+++ Userland/SampleCodeModule/include/input.h
@@ -0,0 +1,11 @@
+#ifndef __INPUT_H__
+#define __INPUT_H__
+
+#include <string.h>
+#include <my_process.h>
+#include <utils.h>
+#include <lib.h>
+
+void cat(int argc, char *argv[]);
+
+#endif
---
+++ Userland/SampleCodeModule/input.c
@@ -0,0 +1,18 @@
+#include <input.h>
+
+#define MAX_BUF_SIZE 256
+
+void cat(void) {
+//     int32_t counter = 0;
+//   int64_t c;
+//   while ((c = get_char()) != -1) {
+//       put_char(STDOUT_FILENO, c);
+//     } else {
+//       if (c == '\b')
+//         counter--;
+//       else
+//         couner++;
+//       put_char(STDOUT_FILENO, c);t
+//     }
+    exit();
+}
