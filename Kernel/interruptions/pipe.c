bb712fb7ae45d76b29129f262f3b295d9a5fc401
BraveJero <jbrave271@gmail.com>
2021-10-31 08:51:28 -0300

    remove user validation

--- Kernel/interruptions/pipe.c
+++ Kernel/interruptions/pipe.c
@@ -6,7 +6,6 @@ typedef struct pipe {
   char buf[BUFFER_SIZE];
   uint8_t r_pointer, w_pointer;
   uint16_t write, lock;
-  pid_t users[MAX_USERS];
   uint8_t userCount;
 } Pipe;
 
@@ -30,7 +29,7 @@ int pipe(int16_t pipeID, int fds[2]) {
     if (pipeID < 0)
       return pipeID;
   } else if (pipes[pipeID]) {
-    pipes[pipeID]->users[pipes[pipeID]->userCount++] = getCurrentPid();
+    pipes[pipeID]->userCount++;
     fds[0] = 2 * pipeID + 3;
     fds[1] = 2 * pipeID + 4;
     pipes[pipeID]->write = sem_open(MAX_USER_SEMS + pipeID, 0);
@@ -63,26 +62,17 @@ int pipe(int16_t pipeID, int fds[2]) {
     return -1;
   }
 
-  pipes[pipeID]->users[pipes[pipeID]->userCount++] = getCurrentPid();
+  pipes[pipeID]->userCount++;
   fds[0] = 2 * pipeID + 3; // Avoid 0, 1 and 2 since they belong to 
   fds[1] = 2 * pipeID + 4; // STDIN, STDOUT and STDERR respectively.
   return pipes[pipeID]->userCount;
-}
-
-static int isInUsers(uint8_t pipeID, pid_t pid) {
-  for (uint8_t i = 0; i < MAX_USERS; i++)
-    if (pipes[pipeID]->users[i] == pid)
-      return 1;
-
-  return 0;
 }
 
 int pipeRead(int fd, char *buf, size_t count) {
   fd -= 3;
   if (buf == NULL || count == 0 || fd < 0)
     return -1;
-  if (fd % 2 || pipes[fd / 2] == NULL ||
-      !isInUsers(fd / 2, getCurrentPid())) { // TODO: Make a func isValid
+  if (fd % 2 || pipes[fd / 2] == NULL) {
     return -1;
   }
   fd = fd / 2;
@@ -126,8 +116,7 @@ int pipeWrite(int fd, const char *buf, size_t count) {
   fd -= 3;
   if (buf == NULL || count == 0 || fd < 0)
     return -1;
-  if (fd % 2 == 0 || pipes[(fd - 1) / 2] == NULL ||
-      !isInUsers((fd - 1) / 2, getCurrentPid())) {
+  if (fd % 2 == 0 || pipes[(fd - 1) / 2] == NULL) {
     return -1;
   }
   fd = (fd - 1) / 2; // TODO: Check truncation.
@@ -159,17 +148,6 @@ int closePipe(uint8_t pipeID) {
     return -1;
   }
   if (pipes[pipeID]->userCount > 1) {
-    pid_t currentPid = getCurrentPid();
-    uint8_t i = 0;
-    while (pipes[pipeID]->users[i] != currentPid &&
-           i < pipes[pipeID]->userCount)
-      i++;
-    if (i == pipes[pipeID]->userCount)
-      return -1;
-    while (i < pipes[pipeID]->userCount - 1) {
-      pipes[pipeID]->users[i] = pipes[pipeID]->users[i + 1];
-      i++;
-    }
     pipes[pipeID]->userCount--;
     sem_close(pipes[pipeID]->lock);
     sem_close(pipes[pipeID]->write);
