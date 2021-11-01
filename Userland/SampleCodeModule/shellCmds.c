e3fe10ca7313043983e27a094ce92200d21283f8
juan <juanigarcia@itba.edu.ar>
2021-11-01 18:30:09 -0300

    Add printArgs to help table.

--- Userland/SampleCodeModule/shellCmds.c
+++ Userland/SampleCodeModule/shellCmds.c
@@ -14,7 +14,8 @@ void helpCmd(void) {
   print_f(STDOUT_FILENO, " - filter: Filters the vowels out of the standar input.\n");
   print_f(STDOUT_FILENO, " - pipe: Displays a list of all open pipes.\n");
   print_f(STDOUT_FILENO, " - phylo: Implements the Dining Philosophers.\n");
-  print_f(STDOUT_FILENO, " - loop <argv>: Prints pid, argc and argv after a given amount of time.\n");
+  print_f(STDOUT_FILENO, " - loop: Prints pid once every timer tick.\n");
+  print_f(STDOUT_FILENO, " - printArgs <argv>: Prints argc and argv.\n");
   print_f(STDOUT_FILENO, " - testPrio: Tests priority.\n");
   print_f(STDOUT_FILENO, " - testSync: Tests synchronization.\n");
   print_f(STDOUT_FILENO, " - testMM: Tests memory manager.\n");
